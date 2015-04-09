48e190c34740233f36dfa6da45a9c90cc835d5f0
Yannick Francois <yannick.francois@free.fr>
2015-04-09 11:55:27 +0200

    Account controller cant be acceed when not login

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,6 +1,7 @@
 class AccountsController < AppController
   def create
   end
+
   def edit
     @account = Account.find(Account.current_id)
   end
@@ -15,9 +16,8 @@ class AccountsController < AppController
   end
 
   def import_samples
-    # @job_id = XmlImportWorker.perform_async(Account.current_id, ENV["SAMPLES_XML_DIR"], ENV["SAMPLES_XML_FILE"], nil)
     @job_id = SamplesImportWorker.perform_async(Account.current_id)
     render 'contacts/import_samples'
   end
-  
+
 end
